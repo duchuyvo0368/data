acd9e5cbf128f8a9db2a24c2dc83ea9e78ef44e2
duchuyvo0368 <voduchuy016688@gmail.com>
2024-11-16 00:15:28 +0700

    add

--- ecom.sql
+++ ecom.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 14, 2024 lúc 03:42 PM
+-- Thời gian đã tạo: Th10 15, 2024 lúc 06:14 PM
 -- Phiên bản máy phục vụ: 10.4.32-MariaDB
 -- Phiên bản PHP: 8.0.30
 
